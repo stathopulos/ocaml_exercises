45d0c50c2ab74bc0c5695907b82078f327a649ec
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2024-10-10 22:32:28 -0600

    Exercise 2

--- lib/exercises.ml
+++ lib/exercises.ml
@@ -1,1 +1,6 @@
 let rec last = function [] -> None | [ x ] -> Some x | _ :: xs -> last xs
+
+let rec last_two = function
+  | [ x; y ] -> Some (x, y)
+  | _ :: x :: xs -> last_two (x :: xs)
+  | _ -> None
--- test/test_ocaml_exercises.ml
+++ test/test_ocaml_exercises.ml
@@ -1,6 +1,15 @@
 open OUnit2
 open Ocaml_exercises.Exercises
 
-let tail_of_list _ = assert_equal (Some "d") (last [ "a"; "b"; "c"; "d" ])
-let tests = "Exercise Tests" >::: [ "tail_of_list" >:: tail_of_list ]
+let lst = [ "a"; "b"; "c"; "d" ]
+let tail_of_list _ = assert_equal (Some "d") (last lst)
+let last_two_of_list _ = assert_equal (Some ("c", "d")) (last_two lst)
+
+let tests =
+  "Exercise Tests"
+  >::: [
+         "tail_of_list" >:: tail_of_list;
+         "last_two_of_list" >:: last_two_of_list;
+       ]
+
 let () = run_test_tt_main tests
