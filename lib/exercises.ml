1dc5b1526696dac0ab2b957ecfab3f18f0fbc466
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2024-10-10 22:58:29 -0600

    Exercise 3

--- lib/exercises.ml
+++ lib/exercises.ml
@@ -4,3 +4,8 @@ let rec last_two = function
   | [] | [ _ ] -> None
   | [ x; y ] -> Some (x, y)
   | _ :: t -> last_two t
+
+let rec my_nth lst n =
+  match lst with
+  | [] -> None
+  | x :: xs -> if n = 0 then Some x else my_nth xs (n - 1)
--- test/test_ocaml_exercises.ml
+++ test/test_ocaml_exercises.ml
@@ -4,12 +4,14 @@ open Ocaml_exercises.Exercises
 let lst = [ "a"; "b"; "c"; "d" ]
 let tail_of_list _ = assert_equal (Some "d") (last lst)
 let last_two_of_list _ = assert_equal (Some ("c", "d")) (last_two lst)
+let nth_element _ = assert_equal (Some "c") (my_nth lst 2)
 
 let tests =
   "Exercise Tests"
   >::: [
          "tail_of_list" >:: tail_of_list;
          "last_two_of_list" >:: last_two_of_list;
+         "nth_element" >:: nth_element;
        ]
 
 let () = run_test_tt_main tests
