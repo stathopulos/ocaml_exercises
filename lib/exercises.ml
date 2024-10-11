be72364920a40636b3a99800f4ed8708a3679185
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2024-10-10 22:35:51 -0600

    Exercise 2 more explicit and readable solution

--- lib/exercises.ml
+++ lib/exercises.ml
@@ -1,6 +1,6 @@
 let rec last = function [] -> None | [ x ] -> Some x | _ :: xs -> last xs
 
 let rec last_two = function
+  | [] | [ _ ] -> None
   | [ x; y ] -> Some (x, y)
-  | _ :: x :: xs -> last_two (x :: xs)
-  | _ -> None
+  | _ :: t -> last_two t
