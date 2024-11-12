b97ea6627144d3dc0bcbf4c78944f95990593cdd
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2024-11-11 21:19:54 -0700

    Exercise 8

--- lib/exercises.ml
+++ lib/exercises.ml
@@ -31,3 +31,15 @@ let rle lst =
         if a = b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
   in
   List.rev (aux 0 [] lst)
+
+type 'a rle = One of 'a | Many of int * 'a
+let mod_rle lst =
+  let make_tuple count x = if count = 1 then One x else Many (count, x) in
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> make_tuple (count + 1) x :: acc
+    | a :: (b :: _ as t) ->
+        if a = b then aux (count + 1) acc t
+        else aux 0 (make_tuple (count + 1) a :: acc) t
+  in
+  List.rev (aux 0 [] lst)
--- test/test_ocaml_exercises.ml
+++ test/test_ocaml_exercises.ml
@@ -9,6 +9,7 @@ let list_length _ = assert_equal 4 (length lst)
 let rev_list _ = assert_equal [ "d"; "c"; "b"; "a" ] (rev lst)
 let list_palindrome _ = assert_equal true (is_palindrome [ "d"; "a"; "d" ])
 let rle_list _ = assert_equal [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")] (rle ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"])
+let modified_rle _ = assert_equal [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")] (mod_rle ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"])
 
 let tests =
   "Exercise Tests"
@@ -20,6 +21,7 @@ let tests =
          "rev_list" >:: rev_list;
          "list_palindrome" >:: list_palindrome;
          "rle_list" >:: rle_list;
+         "modified_rle" >:: modified_rle;
        ]
 
 let () = run_test_tt_main tests
