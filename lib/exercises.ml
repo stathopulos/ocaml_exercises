80617b277643d5ebb6d3a1e4380567a391560e54
Konstantinos Stathopulos <19920164+stathopulos@users.noreply.github.com>
2024-11-11 20:49:24 -0700

    Exercise 7

--- lib/exercises.ml
+++ lib/exercises.ml
@@ -22,3 +22,12 @@ let rev lst =
 
 let is_palindrome lst = rev lst = lst
 (* let is_palindrome lst = List.rev lst = lst *)
+
+let rle lst =
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> (count + 1, x) :: acc
+    | a :: (b :: _ as t) ->
+        if a = b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
+  in
+  List.rev (aux 0 [] lst)
--- test/test_ocaml_exercises.ml
+++ test/test_ocaml_exercises.ml
@@ -8,6 +8,7 @@ let nth_element _ = assert_equal (Some "c") (my_nth lst 2)
 let list_length _ = assert_equal 4 (length lst)
 let rev_list _ = assert_equal [ "d"; "c"; "b"; "a" ] (rev lst)
 let list_palindrome _ = assert_equal true (is_palindrome [ "d"; "a"; "d" ])
+let rle_list _ = assert_equal [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")] (rle ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"])
 
 let tests =
   "Exercise Tests"
@@ -18,6 +19,7 @@ let tests =
          "list_length" >:: list_length;
          "rev_list" >:: rev_list;
          "list_palindrome" >:: list_palindrome;
+         "rle_list" >:: rle_list;
        ]
 
 let () = run_test_tt_main tests
